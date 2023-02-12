ea69e105d856396be587a401bfafb3291ef1903d
Giada Dall'Ara <giada.25700@gmail.com>
2023-02-12 15:19:00 +0100

    Esercizio corretto

--- Esercizio/src/Start.java
+++ Esercizio/src/Start.java
@@ -1,6 +1,6 @@
 public class Start {
     public static void main(String[] args) {
-        for (int i = 5; i <= 25; i+=5){
+        for (int i = 5; i <= 25; i++){
             System.out.println(i);
         }
     }
